b160ad62dd8a8fa0ad3037b506e00e733557f01c
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-09 22:50:26 +0200

    Add new function 'fetch_dt_doc()'

--- dtweb/client.py
+++ dtweb/client.py
@@ -19,3 +19,19 @@ def fetch_host_url(dtid: str) -> str:
 
     r = requests.get(dtid)
     return r.url
+
+def fetch_dt_doc(dtid: str) -> dict:
+    """
+    Fetches DT doc in dict form based on a DTID.
+
+    Args:
+      dtid: The DT identifier of the target DT. Must be URL.
+
+    Returns:
+      DT doc in python dict form.
+
+    """
+
+    dt_url = fetch_host_url(dtid)
+    r = requests.get(dt_url + '/index.json')
+    return r.json()
