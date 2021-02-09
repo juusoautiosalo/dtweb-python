0652b0bff8f676d7ce0d70a085f212d1fc957817
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-09 18:05:48 +0200

    Bump initial development version down to 0.0.1
    as shown in https://packaging.python.org/tutorials/packaging-projects/

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='dtweb',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.0.1',
     description='Python library for accessing the Digital Twin Web',
     author='Juuso Autiosalo',
     license='MIT',
