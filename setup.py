d2ade14ae66fd92b1f271c02d8c468b0564be845
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-11 15:09:52 +0200

    Assign readme as long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
     name='dtweb',
     version='0.0.1',
     author='Juuso Autiosalo',
     author_email="juuso.autiosalo@iki.fi",
     description='Python library for accessing the Digital Twin Web',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://github.com/juusoautiosalo/dtweb-python",
     classifiers=[
         "Programming Language :: Python :: 3",
